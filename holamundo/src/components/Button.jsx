61c378d6cc51f9cbf9f4af3defcfd30fcbdbbcfc
Jorge Trad <jetradl@gmail.com>
2021-07-19 17:52:47 -0500

    State - Events

--- holamundo/src/components/Button.jsx
+++ holamundo/src/components/Button.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Button = props => {
+class Button extends React.Component {
+    state = {
+        count: 0,
+    }
+    handleClick = () => {
+        this.setState({
+            count: this.state.count + 1,
+        })
+    }
+    render(){
+        const { count } = this.state;
+        return(
+            <div>
+                <h1>Manzanas: { count }</h1>
+                <button type="button" onClick={this.handleClick}>Agregar</button>
+            </div>
+        )
+    }
+}
+
+/* const Button = props => {
     const { text } = props;
     return(
         <div>
-            <button type="button">{props.text}</button>
             <button type="button">{text}</button>
         </div>
     )
-}
+} */
 
 export default Button;
