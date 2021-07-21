b782380534c3d57a02356d15b4d482cd71ff3f9b
Jorge Trad <jetradl@gmail.com>
2021-07-20 22:33:45 -0500

    Adding images with Webpack

--- PlatziVideo/src/containers/App.jsx
+++ PlatziVideo/src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
@@ -11,7 +12,7 @@ const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+    <Categories title='Mi Lista'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
@@ -19,7 +20,19 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
-
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Categories title='Originales de Platzi Video'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Footer />
   </div>
 );
 
